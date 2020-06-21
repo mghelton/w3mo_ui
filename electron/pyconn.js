cde60b0ed0acd42b623aec55a217d3e257f820c9
mghelton <gagehelton@gmail.com>
2020-06-21 01:52:33 -0400

    working with issues

--- electron/pyconn.js
+++ electron/pyconn.js
@@ -1,6 +1,7 @@
 const { ipcMain } = require('electron');
 
 let devices;
+let loaded = false;
 
 function getDevices() {
   var getDevices = require('child_process').spawn('python', ['./getDevices.py']);
@@ -13,47 +14,57 @@ function getDevices() {
       button.id = key;
       button.innerHTML = key;
       button.className = "btn btn-primary local_btn";
+      // button.onclick = "control(key)";
+      //button.disabled = 
+
       document.getElementById("devices").appendChild(button);
       document.getElementById("devices").appendChild(document.createElement("br"));
-      console.log(`${key}: ${value}`);
-      console.log(document.getElementById(key).textContent);
-      document.getElementById(key).addEventListener('click', () => {
+      document.getElementById("devices").appendChild(document.createElement("br"));
+      //console.log(`${key}: ${value}`);
+      /*document.getElementById(key).addEventListener('click', () => {
         let new_state;
-        console.log(key);
-        console.log(devices[key]['state']);
-        console.log(devices);
-        console.log(devices[key]);
         if(devices[key]['state'] == 0){new_state = 1;}
         else{new_state = 0;}
-        console.log(new_state);
-        control(key,devices[key]['ip'],new_state);
+        if(loaded){control(key,devices[key]['ip'],new_state);}
+        if(devices[key]['state'] == 1){document.getElementById(key).className = "btn btn-primary local_btn";}
+        else{document.getElementById(key).className = "btn btn-secondary local_btn";}
+      });
+      */
+      document.getElementById(key).addEventListener('click', () => {
+        control(key)
       });
       document.getElementById(key).dispatchEvent(new Event('click'));
     }
   });
 }
 
-function control(key,ip,state) {
-  var controlDevice = require('child_process').spawn('python', ['./control.py', ip, state]);
-  controlDevice.stdout.on('data', function (data) {
-    //result.textContent = data.toString('utf8');
-    console.log(data.toString('utf8'));
-    devices[key]['state'] = data.toString('utf8');
-    // devices[key].state = int(data.toString('utf8'));
-  });
-    
-  controlDevice.stderr.on('data', (data) => {
-    console.error(`stderr: ${data}`);
-  });
-    
-  controlDevice.on('close', (code) => {
-    console.log(`child process exited with code ${code}`);
-  });
+function control(key) {
+    console.log("RUNNING");
+    if(devices[key]['state'] == 0){state = 1;}
+    else{state = 0;}
+    var controlDevice = require('child_process').spawn('python', ['./control.py', devices[key]['ip'], state]);
+    controlDevice.stdout.on('data', function (data) {
+      //result.textContent = data.toString('utf8');
+      console.log(data.toString('utf8'));
+      devices[key]['state'] = parseInt(data.toString('utf8'));
+      if(devices[key]['state'] == 1){document.getElementById(key).className = "btn btn-primary local_btn";}
+      else{document.getElementById(key).className = "btn btn-secondary local_btn";}
+      // devices[key].state = int(data.toString('utf8'));
+    });
+      
+    controlDevice.stderr.on('data', (data) => {
+      console.error(`stderr: ${data}`);
+    });
+      
+    controlDevice.on('close', (code) => {
+      console.log(`child process exited with code ${code}`);
+    });
 }
 
 //events
 document.addEventListener('DOMContentLoaded', function() {
     getDevices();
+    loaded = true;
 }, false);
 
 btn.addEventListener('click', () => {
@@ -61,4 +72,4 @@ btn.addEventListener('click', () => {
   //sendToPython();
 });
 
-btn.dispatchEvent(new Event('click'));
+//btn.dispatchEvent(new Event('click'));
