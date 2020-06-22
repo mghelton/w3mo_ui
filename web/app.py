47a07e7a11254e3b1010808cc4966e8017259ccc
Gage Helton <ghelton@sudotronics.com>
2020-06-22 19:19:34 +0000

    more work on web

--- web/app.py
+++ web/app.py
@@ -1,16 +1,20 @@
-from flask import Flask, render_template, Response, request, redirect, url_for
-#from waitress import serve
-from w3mo import w3mo
+from flask import Flask, render_template
+from w3mo.w3mo import discover
+import json
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def index():
-    #return render_template('index.html');
-    if(request.method == "GET"):
-        return render_template("index.html")
-        
+    return render_template("index.html")
+
+@app.route("/get")
+def get():
+    x = discover()
+    for key in x:
+        x[key]['state'] = x[key]['obj'].state
+        del(x[key]['obj'])
+    return(json.dumps(x))
+
 if __name__ == '__main__':
-    #serve(host="127.0.0.1",port=9000)
-    #app.run(host="127.0.0.1",port=9999)
     app.run(host="0.0.0.0",port=80)
--- web/static/js/app.js
+++ web/static/js/app.js
@@ -1,61 +1,31 @@
-const { ipcMain } = require('electron');
-
 let devices;
-let loaded = false;
 
 function getDevices() {
-  var getDevices = require('child_process').spawn('python', ['py/getDevices.py']);
-  var docFrag = document.createDocumentFragment();
-  getDevices.stdout.on('data', function (data) {
-    devices = JSON.parse(data.toString('utf8'));
-    console.log(devices);
-    for (let [key, value] of Object.entries(devices)) {
-      //create button object
-      var button = document.createElement('button');
-      button.id = key;
-      button.innerHTML = key;
-      // button.className = "btn btn-success";
-      //add button to div
-      document.getElementById("devices").appendChild(button);
-      document.getElementById("devices").appendChild(document.createElement("br"));
-      document.getElementById("devices").appendChild(document.createElement("br"));
-      //create event listener and set button state
-      document.getElementById(key).addEventListener('click', () => control(key));
-      setButtonState(key);
+    let request = new XMLHttpRequest();
+    request.open("GET", "http://192.168.61.200/get");
+    request.send();
+    request.onload = () => {
+        console.log(request);
+        if(request.status === 200) {
+            devices = JSON.parse(request.response);
+            console.log(devices);
+            for (let [key,value] of Object.entries(devices)) {
+                var button = document.createElement("button");
+                button.id = key;
+                button.innerHTML = key;
+                document.getElementById("devices").appendChild(button);
+                document.getElementById("devices").appendChild(document.createElement("br"));
+                document.getElementById("devices").appendChild(document.createElement("br"));
+                document.getElementById(key).addEventListener('click', () => control(key));
+            }
+            document.getElementById("loader").remove();
+        } else {
+            console.log(`error ${request.status} ${request.statusTest}`);
+        }
     }
-    document.getElementById("loader").remove();
-  });
-}
-
-function setButtonState(key){
-  if(devices[key]['state'] == 1){document.getElementById(key).className = "btn btn-active";}
-  else{document.getElementById(key).className = "btn btn-inactive";}
-}
-
-function control(key) {
-    document.getElementById(key).className = "btn btn-wait";
-    if(devices[key]['state'] == 0){state = 1;}
-    else{state = 0;}
-    var controlDevice = require('child_process').spawn('python', ['py/control.py', devices[key]['ip'], state]);
-    controlDevice.stdout.on('data', function (data) {
-      //result.textContent = data.toString('utf8');
-      console.log(data.toString('utf8'));
-      devices[key]['state'] = parseInt(data.toString('utf8'));
-      setButtonState(key);
-      // devices[key].state = int(data.toString('utf8'));
-    });
-      
-    controlDevice.stderr.on('data', (data) => {
-      console.error(`stderr: ${data}`);
-    });
-      
-    controlDevice.on('close', (code) => {
-      console.log(`child process exited with code ${code}`);
-    });
 }
 
 //events
 document.addEventListener('DOMContentLoaded', function() {
-    // getDevices();
-    // loaded = true;
+    getDevices();
 }, false);
