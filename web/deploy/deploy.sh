189b838949575fa4006f34fe84d4d098898b769d
mghelton <gagehelton@gmail.com>
2020-06-23 13:51:04 -0400

    add daemon reload to deploy

--- web/deploy/deploy.sh
+++ web/deploy/deploy.sh
@@ -6,6 +6,7 @@ pip3 install -r ../requirements.txt
 sudo cp w3mo /etc/nginx/sites-enabled
 sudo rm -r /etc/nginx/sites-enabled/default
 sudo cp w3mo.service /etc/systemd/system/
+sudo systemctl daemon-reload
 sudo systemctl enable w3mo
 sudo systemctl restart nginx
 sudo systemctl start w3mo
